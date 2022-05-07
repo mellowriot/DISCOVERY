6b8b22ea422b5c913392bb3df7615bc0e015e5ae
mellowriot <45343456+mellowriot@users.noreply.github.com>
2022-05-07 19:43:02 -0400

    Add files via upload

--- stm32f407xx_spi_driver.h
+++ stm32f407xx_spi_driver.h
@@ -1,3 +1,4 @@
+
 /*
  * stm32f407xx_spi_driver.h
  *
@@ -38,7 +39,6 @@
 /*
 Peripheral:		SPI1
 Base address:	0x40013000
-
 Register:			CR1
 Address:			0x40013000
 Value:				null
@@ -177,6 +177,210 @@ typedef struct
 }SPI_HANDLE;
 
 
+//*****************************************************************
+
+/*
+Bit 15 BIDIMODE: Bidirectional data mode enable
+0: 2-line unidirectional data mode selected
+1: 1-line bidirectional data mode selected
+Note: This bit is not used in I2S mode
+*/
+#define Bidirectional_data_mode_enable;//
+
+
+
+/*
+Bit 14 BIDIOE: Output enable in bidirectional mode
+This bit combined with the BIDImode bit selects the direction of transfer in bidirectional mode
+0: Output disabled (receive-only mode)
+1: Output enabled (transmit-only mode)
+Note: This bit is not used in I2S mode.
+In master mode, the MOSI pin is used while the MISO pin is used in slave mode.
+*/
+#define Output_enable_in_bidirectional_mode;//
+
+
+
+/*
+Bit 13 CRCEN: Hardware CRC calculation enable
+0: CRC calculation disabled
+1: CRC calculation enabled
+Note: This bit should be written only when SPI is disabled (SPE = ‘0’) for correct operation.
+It is not used in I2S mode.
+*/
+#define Hardware_CRC_calculation_enable;//
+
+
+
+/*
+Bit 12 CRCNEXT: CRC transfer next
+0: Data phase (no CRC phase)
+1: Next transfer is CRC (CRC phase)
+Note: When the SPI is configured in full duplex or transmitter only modes, CRCNEXT must be
+written as soon as the last data is written to the SPI_DR register.
+When the SPI is configured in receiver only mode, CRCNEXT must be set after the
+second last data reception.
+This bit should be kept cleared when the transfers are managed by DMA.
+It is not used in I2S mode.
+*/
+#define CRC_transfer_next;//
+
+
+
+/*
+Bit 11 DFF: Data frame format
+0: 8-bit data frame format is selected for transmission/reception
+1: 16-bit data frame format is selected for transmission/reception
+Note: This bit should be written only when SPI is disabled (SPE = ‘0’) for correct operation.
+It is not used in I2S mode
+*/
+#define Data_frame_format;//
+
+
+
+/*
+Bit 10 RXONLY: Receive only
+This bit combined with the BIDImode bit selects the direction of transfer in 2-line
+unidirectional mode. This bit is also useful in a multislave system in which this particular
+slave is not accessed, the output from the accessed slave is not corrupted.
+0: Full duplex (Transmit and receive)
+1: Output disabled (Receive-only mode)
+Note: This bit is not used in I2S mode
+*/
+#define Receive_only;//
+#define FULL_DUPLEX_ENBALE
+#define HALF_DUPLEDX_ENABLE
+
+
+/*
+Bit 9 SSM: Software slave management
+When the SSM bit is set, the NSS pin input is replaced with the value from the SSI bit.
+0: Software slave management disabled
+1: Software slave management enabled
+Note: This bit is not used in I2S mode and SPI TI mode
+*/
+#define Software_slave_management;//
+#define SSM_ENABLE
+#define SSM_DISABLE
+
+
+
+/*
+Bit 8 SSI: Internal slave select
+This bit has an effect only when the SSM bit is set. The value of this bit is forced onto the
+NSS pin and the IO value of the NSS pin is ignored.
+Note: This bit is not used in I2S mode and SPI TI mode
+*/
+#define Internal_slave_select;//
+#define CHECK_IF_SSM_ENABLED;
+#define INTERNAL_SLAVE_SELECT_ENABLE
+
+
+/*
+Bit 7 LSBFIRST: Frame format
+0: MSB transmitted first
+1: LSB transmitted first
+Note: This bit should not be changed when communication is ongoing.
+It is not used in I2S mode and SPI TI mode
+*/
+#define Frame_format;//
+#define BIG_ENDIAN_ENABLE
+#define LITTLE_ENDIAN_ENABLE
+
+
+
+/*
+Bit 6 SPE: SPI enable
+0: Peripheral disabled
+1: Peripheral enabled
+Note: This bit is not used in I2S mode.
+When disabling the SPI, follow the procedure described in Section 28.3.8.
+*/
+#define SPI_enable;//
+#define SPI_PERIPHERAL_ENABLE
+#define SPI_PERIPHERAL_DISABLE
+
+
+
+/*
+Bits 5:3 BR[2:0]: Baud rate control
+000: fPCLK/2
+001: fPCLK/4
+010: fPCLK/8
+011: fPCLK/16
+100: fPCLK/32
+101: fPCLK/64
+110: fPCLK/128
+111: fPCLK/256
+Note: These bits should not be changed when communication is ongoing.
+They are not used in I2S mode.
+*/
+#define Baud_rate_control;//
+#define BAUD_DEVIDE_BY_2
+#define BAUD_DEVIDE_BY_4
+#define BAUD_DEVIDE_BY_8
+#define BAUD_DEVIDE_BY_16
+#define BAUD_DEVIDE_BY_32
+#define BAUD_DEVIDE_BY_64
+#define BAUD_DEVIDE_BY_128
+#define BAUD_DEVIDE_BY_256
+
+
+/*
+Bit 2 MSTR: Master selection
+0: Slave configuration
+1: Master configuration
+Note: This bit should not be changed when communication is ongoing.
+It is not used in I2S mode
+*/
+#define Master_selection;//
+#define MASTER_AS_MASTER_ENABLE;
+#define SLAVE_AS_MASTER_ENABLE;
+
+/*
+Bit1 CPOL: Clock polarity
+0: CK to 0 when idle
+1: CK to 1 when idle
+Note: This bit should not be changed when communication is ongoing.
+It is not used in I2S mode and SPI TI mode.
+*/
+#define Clock_polarity;//
+#define CLOCK_HIGH_WHEN_IDLE_ENABLE;
+#define CLOCK_LOW_WHEN_IDLE_ENABLE;
+
+
+
+/*
+Bit 0 CPHA: Clock phase
+0: The first clock transition is the first data capture edge
+1: The second clock transition is the first data capture edge
+Note: This bit should not be changed when communication is ongoing.
+It is not used in I2S mode and SPI TI mode.
+*/
+#define Clock_phase;//
+#define FIRST_EDGE_DATA_CAPTURE_ENABLE;
+#define SECOND_EDGE_DATA_CAPTURE_ENABLE;
+
+
+/*
+Bits 15:0 DR[15:0]: Data register
+Data received or to be transmitted.
+The data register is split into 2 buffers - one for writing (Transmit Buffer) and another one for
+reading (Receive buffer). A write to the data register will write into the Tx buffer and a read
+from the data register will return the value held in the Rx buffer.
+
+Note: These notes apply to SPI mode:
+Depending on the data frame format selection bit (DFF in SPI_CR1 register), the data
+sent or received is either 8-bit or 16-bit. This selection has to be made before enabling
+the SPI to ensure correct operation.
+
+For an 8-bit data frame, the buffers are 8-bit and only the LSB of the register
+(SPI_DR[7:0]) is used for transmission/reception. When in reception mode, the MSB of
+the register (SPI_DR[15:8]) is forced to 0.
+For a 16-bit data frame, the buffers are 16-bit and the entire register, SPI_DR[15:0] is
+used for transmission/reception.
+*/
+#define Data_register;//
 
 
 
@@ -189,6 +393,47 @@ typedef struct
 
 
 
+
+
+
+//**************************************************************
+
+#define TI_frame_format_error;//
+
+#define Busy_flag;//
+
+#define Overrun_flag;//
+
+#define Mode_fault;//
+
+#define CRC_error_flag;//
+
+#define Underrun_flag;//
+
+#define Channel_side;//
+
+#define Transmit_buffer_empty;//
+
+#define Receive_buffer_not_empty;//
+
+
+//**************************************************************
+
+
+
+// ? // SPI_TypeDef                *Instance;      /*!< SPI registers base address               */
+
+// ? // SPI_InitTypeDef            Init;           /*!< SPI communication parameters             */
+
+// ? // uint8_t                    *pTxBuffPtr;    /*!< Pointer to SPI Tx transfer Buffer        */
+
+#define                   TxXferSize;     /*!< SPI Tx Transfer size                     */
+
+//__IO uint16_t              TxXferCount;    /*!< SPI Tx Transfer Counter                  */
+
+// ? // uint8_t                    *pRxBuffPtr;    /*!< Pointer to SPI Rx transfer Buffer        */
+
+#define                   RxXferSize;     /*!< SPI Rx Transfer size                     */
 
 
 
